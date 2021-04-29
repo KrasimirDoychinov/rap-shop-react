87ff3c791ddea9befe5ad9a4c793ea4e2e118d8a
Krasimir D <detha4545@gmail.com>
2021-04-29 14:34:20 +0300

    Install Redux and React-router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import Backendless from 'backendless';
+
+const APP_ID = '95DD6575-7321-470D-9574-05E6A27ADA04';
+const API_KEY = '2AB164E3-2144-46CF-A945-46D3532B602A';
+Backendless.serverURL = 'https://eu-api.backendless.com';
+Backendless.initApp(APP_ID, API_KEY);
+
+
+
 function App() {
   return (
     <div>
